474b943d352cddeb3210cc02e0558a0cdf06394c
Glavo <zjx001202@gmail.com>
2023-09-17 20:31:35 +0800

    Read metadata

--- japp-reader/src/main/java/org/glavo/japp/reader/JAppReader.java
+++ japp-reader/src/main/java/org/glavo/japp/reader/JAppReader.java
@@ -1,11 +1,15 @@
 package org.glavo.japp.reader;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.Closeable;
 import java.io.EOFException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,7 +17,7 @@ public final class JAppReader implements Closeable {
     public static final short MAJOR_VERSION = -1;
     public static final short MINOR_VERSION = 0;
 
-    public static final int FILE_END_LENGTH = 48;
+    public static final int FILE_END_SIZE = 48;
 
     private final ReentrantLock lock = new ReentrantLock();
 
@@ -27,7 +31,7 @@ public final class JAppReader implements Closeable {
         try {
             long fileSize = channel.size();
 
-            if (fileSize < FILE_END_LENGTH) {
+            if (fileSize < FILE_END_SIZE) {
                 throw new IOException("File is too small");
             }
 
@@ -43,7 +47,7 @@ public final class JAppReader implements Closeable {
                 throw new EOFException();
             }
 
-            endBuffer.limit(endBufferSize).position(endBufferSize - FILE_END_LENGTH);
+            endBuffer.limit(endBufferSize).position(endBufferSize - FILE_END_SIZE);
 
             int magicNumber = endBuffer.getInt();
             if (magicNumber != 0x5050414a) {
@@ -68,17 +72,37 @@ public final class JAppReader implements Closeable {
                 throw new IOException("Unsupported flag");
             }
 
-            if (fileContentSize > fileSize || fileContentSize < FILE_END_LENGTH) {
+            if (fileContentSize > fileSize || fileContentSize < FILE_END_SIZE) {
                 throw new IOException("Invalid file size: " + fileContentSize);
             }
 
-            if (metadataOffset >= fileContentSize - FILE_END_LENGTH) {
+            if (metadataOffset >= fileContentSize - FILE_END_SIZE) {
                 throw new IOException("Invalid metadata offset: " + metadataOffset);
             }
 
             this.contentOffset = fileSize - fileContentSize;
 
-            // TODO
+            long metadataSize = fileContentSize - FILE_END_SIZE - metadataOffset;
+
+            JSONObject json;
+            if (metadataSize < endBufferSize - FILE_END_SIZE) {
+                json = new JSONObject(new String(endBuffer.array(), (int) (endBufferSize - metadataSize - FILE_END_SIZE), (int) metadataSize, StandardCharsets.UTF_8));
+            } else {
+                if (metadataSize > (1 << 30)) {
+                    throw new IOException("Metadata is too large");
+                }
+
+                ByteBuffer metadataBuffer = ByteBuffer.allocate((int) metadataSize);
+                channel.position(contentOffset + metadataOffset);
+                while (channel.read(metadataBuffer) > 0) {
+                }
+
+                if (metadataBuffer.remaining() > 0) {
+                    throw new EOFException();
+                }
+
+                json = new JSONObject(new String(metadataBuffer.array(), StandardCharsets.UTF_8));
+            }
 
         } catch (Throwable e) {
             try {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "japp"
 include(
     "packer",
     "japp-reader",
+    "japp-filesystem",
     "boot-loader",
     "boot-loader-java8",
 )
