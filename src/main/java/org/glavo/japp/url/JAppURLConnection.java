c3cea14b7ec0f335a4353c3d10fbcb5ffe96e0c8
Glavo <zjx001202@gmail.com>
2023-09-23 20:02:36 +0800

    Check JAppURL

--- src/main/java/org/glavo/japp/url/JAppURLConnection.java
+++ src/main/java/org/glavo/japp/url/JAppURLConnection.java
@@ -1,13 +1,19 @@
 package org.glavo.japp.url;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class JAppURLConnection extends URLConnection {
 
-    protected JAppURLConnection(URL url) {
+    JAppURLConnection(URL url) throws MalformedURLException {
         super(url);
+
+        String path = url.getPath();
+        if (path.isEmpty() || path.charAt(0) != '/') {
+            throw new MalformedURLException(url + " missing path or /");
+        }
     }
 
     @Override
