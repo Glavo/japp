dfeeff1340fdb8f85024e99c40d47fee5f095667
Glavo <zjx001202@gmail.com>
2023-10-16 17:32:51 +0800

    Impl add opens and add exports

--- src/main/java/org/glavo/japp/launcher/BootLauncher.java
+++ src/main/java/org/glavo/japp/launcher/BootLauncher.java
@@ -2,6 +2,7 @@ package org.glavo.japp.launcher;
 
 import jdk.internal.loader.BuiltinClassLoader;
 import jdk.internal.loader.URLClassPath;
+import jdk.internal.module.Modules;
 import org.glavo.japp.JAppClasspathItem;
 import org.glavo.japp.JAppReader;
 import org.glavo.japp.module.JAppModuleFinder;
@@ -14,6 +15,43 @@ import java.util.Collections;
 import java.util.Map;
 
 public final class BootLauncher {
+    private static void addExportsOrOpens(ModuleLayer layer, String item, boolean open) {
+        int idx0 = item.indexOf('/');
+
+        if (idx0 <= 0) {
+            throw new IllegalArgumentException(item);
+        }
+
+        Module source = layer.findModule(item.substring(0, idx0)).get();
+
+        int idx1 = item.indexOf('=', idx0 + 1);
+        if (idx1 <= idx0 + 1 || idx1 == item.length() - 1) {
+            throw new IllegalArgumentException(item);
+        }
+
+        String packageName = item.substring(idx0 + 1, idx1);
+
+        String[] targets = item.substring(idx1 + 1).split(",");
+
+        for (String targetName : targets) {
+            Module target = targetName.equals("ALL-UNNAMED") ? null : layer.findModule(targetName).get();
+
+            if (open) {
+                if (target == null) {
+                    Modules.addOpensToAllUnnamed(source, packageName);
+                } else {
+                    Modules.addOpens(source, packageName, target);
+                }
+            } else {
+                if (target == null) {
+                    Modules.addExportsToAllUnnamed(source, packageName);
+                } else {
+                    Modules.addExports(source, packageName, target);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Throwable {
         JAppReader reader = JAppReader.getSystemReader();
 
@@ -49,7 +87,15 @@ public final class BootLauncher {
             throw new UnsupportedOperationException("TODO");
         }
 
-        // TODO: Add-Opens and Add-Exports
+        // TODO: Add-Reads
+
+        for (String item : reader.getAddOpens()) {
+            addExportsOrOpens(controller.layer(), item, true);
+        }
+
+        for (String item : reader.getAddExports()) {
+            addExportsOrOpens(controller.layer(), item, false);
+        }
 
         Class<?> mainClass = Class.forName(mainClassName, false, loader);
         if (mainClass.getModule().isNamed()) {
