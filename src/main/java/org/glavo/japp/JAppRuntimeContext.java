0d3892dc0d016ade96aef975c7caeb68c19b2097
Glavo <zjx001202@gmail.com>
2023-10-22 01:07:40 +0800

    Use java.version property

--- src/main/java/org/glavo/japp/JAppRuntimeContext.java
+++ src/main/java/org/glavo/japp/JAppRuntimeContext.java
@@ -3,9 +3,7 @@ package org.glavo.japp;
 public final class JAppRuntimeContext {
     
     public static JAppRuntimeContext fromCurrentEnvironment() {
-        @SuppressWarnings("deprecation")
-        int release = Runtime.version().major();
-        return new JAppRuntimeContext(release);
+        return new JAppRuntimeContext(Integer.getInteger("java.version"));
     }
     
     private final int release;
