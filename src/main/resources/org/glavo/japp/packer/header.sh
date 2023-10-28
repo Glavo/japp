73474549abbd3feac79372a808de7eb275cd3b5a
Glavo <zjx001202@gmail.com>
2023-10-28 17:59:49 +0800

    Update documents

--- src/main/resources/org/glavo/japp/packer/header.sh
+++ src/main/resources/org/glavo/japp/packer/header.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# TODO: In future we should look for japp launcher in the PATH
 exec '%java.home%/bin/java' --module-path '%japp.launcher%' --module org.glavo.japp/org.glavo.japp.launcher.Launcher "${BASH_SOURCE[0]}" "$@"
