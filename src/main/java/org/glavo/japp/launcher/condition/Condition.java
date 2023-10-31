9e8d9e33657125045cf44d5254690ab4d4cf59de
Glavo <zjx001202@gmail.com>
2023-10-31 15:53:26 +0800

    Cleanup code

--- src/main/java/org/glavo/japp/launcher/condition/Condition.java
+++ src/main/java/org/glavo/japp/launcher/condition/Condition.java
@@ -1,28 +1,9 @@
 package org.glavo.japp.launcher.condition;
 
-import org.glavo.japp.thirdparty.json.JSONObject;
-
 import java.util.function.Predicate;
 
 public interface Condition extends Predicate<JAppRuntimeContext> {
-    static Condition fromJson(JSONObject obj) {
-        String type = obj.getString("Type");
-
-        switch (type) {
-            case AndCondition.TYPE:
-                return AndCondition.fromJson(obj);
-            case OrCondition.TYPE:
-                return OrCondition.fromJson(obj);
-            case JavaVersionCondition.TYPE:
-                return JavaVersionCondition.fromJson(obj);
-            default:
-                throw new IllegalArgumentException("Unknown Type: " + type);
-        }
-    }
-
     String type();
-
-    JSONObject toJson();
 
     boolean test(JAppRuntimeContext context);
 }
