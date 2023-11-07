e17180d08a45e61d66450def0a8607800696cfa5
Glavo <zjx001202@gmail.com>
2023-11-07 23:32:18 +0800

    Delete glob-library-java

--- boot/src/main/java/org/glavo/japp/boot/fs/JAppFileSystem.java
+++ boot/src/main/java/org/glavo/japp/boot/fs/JAppFileSystem.java
@@ -1,7 +1,5 @@
 package org.glavo.japp.boot.fs;
 
-import org.glavo.japp.thirdparty.glob.GlobPattern;
-import org.glavo.japp.thirdparty.glob.MatchingEngine;
 import org.glavo.japp.boot.JAppReader;
 
 import java.io.IOException;
@@ -96,15 +94,18 @@ public final class JAppFileSystem extends FileSystem {
         }
         String syntax = syntaxAndPattern.substring(0, pos);
         String input = syntaxAndPattern.substring(pos + 1);
+
+        String regex;
         if (syntax.equalsIgnoreCase("glob")) {
-            final MatchingEngine glob = GlobPattern.compile(input);
-            return path -> glob.matches(path.toString());
+            regex = convertGlobToRegex(input);
         } else if (syntax.equalsIgnoreCase("regex")) {
-            final Pattern pattern = Pattern.compile(input);
-            return path -> pattern.matcher(path.toString()).matches();
+            regex = input;
         } else {
             throw new UnsupportedOperationException("Syntax '" + syntax + "' not recognized");
         }
+
+        final Pattern pattern = Pattern.compile(regex);
+        return path -> pattern.matcher(path.toString()).matches();
     }
 
     @Override
@@ -115,5 +116,95 @@ public final class JAppFileSystem extends FileSystem {
     @Override
     public WatchService newWatchService() throws IOException {
         throw new UnsupportedOperationException();
+    }
+
+    // https://stackoverflow.com/a/17369948/7659948
+    private static String convertGlobToRegex(String pattern) {
+        StringBuilder sb = new StringBuilder(pattern.length());
+        int inGroup = 0;
+        int inClass = 0;
+        int firstIndexInClass = -1;
+        char[] arr = pattern.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char ch = arr[i];
+            switch (ch) {
+                case '\\':
+                    if (++i >= arr.length) {
+                        sb.append('\\');
+                    } else {
+                        char next = arr[i];
+                        switch (next) {
+                            case ',':
+                                // escape not needed
+                                break;
+                            case 'Q':
+                            case 'E':
+                                // extra escape needed
+                                sb.append('\\');
+                            default:
+                                sb.append('\\');
+                        }
+                        sb.append(next);
+                    }
+                    break;
+                case '*':
+                    if (inClass == 0)
+                        sb.append(".*");
+                    else
+                        sb.append('*');
+                    break;
+                case '?':
+                    if (inClass == 0)
+                        sb.append('.');
+                    else
+                        sb.append('?');
+                    break;
+                case '[':
+                    inClass++;
+                    firstIndexInClass = i+1;
+                    sb.append('[');
+                    break;
+                case ']':
+                    inClass--;
+                    sb.append(']');
+                    break;
+                case '.':
+                case '(':
+                case ')':
+                case '+':
+                case '|':
+                case '^':
+                case '$':
+                case '@':
+                case '%':
+                    if (inClass == 0 || (firstIndexInClass == i && ch == '^'))
+                        sb.append('\\');
+                    sb.append(ch);
+                    break;
+                case '!':
+                    if (firstIndexInClass == i)
+                        sb.append('^');
+                    else
+                        sb.append('!');
+                    break;
+                case '{':
+                    inGroup++;
+                    sb.append('(');
+                    break;
+                case '}':
+                    inGroup--;
+                    sb.append(')');
+                    break;
+                case ',':
+                    if (inGroup > 0)
+                        sb.append('|');
+                    else
+                        sb.append(',');
+                    break;
+                default:
+                    sb.append(ch);
+            }
+        }
+        return sb.toString();
     }
 }
--- boot/src/main/java/org/glavo/japp/thirdparty/package-info.java
+++ boot/src/main/java/org/glavo/japp/thirdparty/package-info.java
@@ -2,7 +2,6 @@
  * Some third-party libraries are temporarily used and will be replaced in the future:
  *
  *   - JSON in Java: https://github.com/stleary/JSON-java
- *   - glob-library-java: https://github.com/hrakaroo/glob-library-java
  *   - lz4-java: https://github.com/lz4/lz4-java
  */
 package org.glavo.japp.thirdparty;
