ac194cac1e7d28bc97f1cb3270320802c18efe89
Glavo <zjx001202@gmail.com>
2023-12-04 09:40:47 +0800

    Add license

--- boot/src/main/java/org/glavo/japp/boot/module/JAppModuleFinder.java
+++ boot/src/main/java/org/glavo/japp/boot/module/JAppModuleFinder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.glavo.japp.boot.module;
 
 import org.glavo.japp.boot.JAppReader;
@@ -11,8 +26,6 @@ import java.lang.module.FindException;
 import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
 import java.lang.module.ModuleReference;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Supplier;
