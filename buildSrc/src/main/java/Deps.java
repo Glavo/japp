b0ef26f959a49e2faddc33adae16c01a3e46be90
Glavo <zjx001202@gmail.com>
2023-12-22 20:41:31 +0800

    Bump zstd-jni to 1.5.5-11

--- buildSrc/src/main/java/Deps.java
+++ buildSrc/src/main/java/Deps.java
@@ -3,5 +3,5 @@ public final class Deps {
     }
 
     public static final String AIRCOMPRESSOR = "io.airlift:aircompressor:0.25";
-    public static final String ZSTD_JNI = "com.github.luben:zstd-jni:1.5.5-10";
+    public static final String ZSTD_JNI = "com.github.luben:zstd-jni:1.5.5-11";
 }
