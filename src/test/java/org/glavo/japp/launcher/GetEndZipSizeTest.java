187df5308eb77d6dbcc26a620173aa431efa8f65
Glavo <zjx001202@gmail.com>
2024-01-09 23:56:04 +0800

    Create GetEndZipSizeTest

---
+++ src/test/java/org/glavo/japp/launcher/GetEndZipSizeTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glavo.japp.launcher;
+
+import com.github.luben.zstd.Zstd;
+import org.glavo.japp.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetEndZipSizeTest {
+
+    private static Path getJarPath(Class<?> clazz) {
+        try {
+            return Path.of(clazz.getProtectionDomain().getCodeSource().getLocation().toURI());
+        } catch (URISyntaxException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    private static Stream<Path> testFiles() {
+        return Stream.of(Test.class, Zstd.class).map(GetEndZipSizeTest::getJarPath);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testFiles")
+    void test(Path zipFile) throws IOException {
+        try (FileChannel channel = FileChannel.open(zipFile)) {
+            long fileSize = channel.size();
+
+            int endBufferSize = (int) Math.max(fileSize, 8192);
+            ByteBuffer endBuffer = ByteBuffer.allocate(endBufferSize).order(ByteOrder.LITTLE_ENDIAN);
+
+            channel.position(fileSize - endBufferSize);
+            IOUtils.readFully(channel, endBuffer);
+
+            endBuffer.flip();
+
+            assertEquals(fileSize, JAppLauncherMetadata.getEndZipSize(endBuffer));
+        }
+    }
+}
